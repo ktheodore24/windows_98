8fed6887f38ab1741e4c5e77485898add0b5e45f
ktheodore24 <kennytheodo@gmail.com>
2021-12-17 03:31:15 -0500

    playing around with js and css

--- js/main.js
+++ js/main.js
@@ -2,9 +2,3 @@
 const leaveWebsite = document.querySelector(".close-button");
 
 leaveWebsite.addEventListener("click", closeTab);
-
-function closeTab() {
-    window.open('/Employees/GetAttendenceDataBySelectedData/?attendDateString=' + $("#AttendDate").val() + ', "_blank');
-    
-    window.top.close();
-}
