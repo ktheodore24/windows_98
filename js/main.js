67a840d6739caa71ad778b8bb7ea46d4136f6ff2
ktheodore24 <kennytheodo@gmail.com>
2021-12-20 13:58:36 -0500

    working on some javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 // Close button function
 const leaveWebsite = document.querySelector(".close-button");
+const win = 
 
 leaveWebsite.addEventListener("click", closeTab);
+
+function closeTab() {
+    window.open(url, '_blank').focus();
+}
